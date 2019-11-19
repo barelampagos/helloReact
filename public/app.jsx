d5c8c14d5fc2e5f68b9871c6a0183e401fc12c54
barelampagos <barelampagos@gmail.com>
2019-11-18 20:10:24 -0800

    Adding Form + Button + onClick function

--- public/app.jsx
+++ public/app.jsx
@@ -4,7 +4,7 @@ var Greeter = React.createClass({
 
 	// render - expects JSX code that gets rendered to browser
 	render: function() {
-		// Grabs name from props
+		// Grabs fields from props
 		var name = this.props.name;
 		var message = this.props.message;
 
@@ -13,15 +13,29 @@ var Greeter = React.createClass({
 			<div>
 				<h1>Hello {name}!</h1>
 				<p>{message}</p>
+
+				<form onSubmit={this.onButtonClick}>
+					<input type='text' ref='name' />
+					<button>Set Name</button>
+				</form>
 			</div>
 		);
 	},
-	// Set default props for use in app if none passed in at render
+	// getDefaultProps - Set default props for use in app if none passed in at render
 	getDefaultProps: function() {
 		return {
 			name: 'React',
 			message: 'Component'
 		};
+	},
+	// Custom app method
+	onButtonClick: function(e) {
+		// Prevents default behavior of form submit & page refresh
+		e.preventDefault();
+
+		// Fetches value from HTML element with ref = name
+		var name = this.refs.name.value;
+		alert(name);
 	}
 });
 
