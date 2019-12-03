2391e283fd33a010f83048ad38d397d48820932e
barelampagos <barelampagos@gmail.com>
2019-12-02 20:40:58 -0800

    Adding JSX + babel-loader & making old app work

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	entry: './public/app.js', // Entry point of application
+	entry: './public/app.jsx', // Entry point of application
 	output: {
 		// Output of bundled file
 		path: __dirname, // Node: gives us current dir with __dirname
@@ -8,5 +8,21 @@ module.exports = {
 	resolve: {
 		// List of file extensions we should be able to process
 		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders: [
+			{
+				// Initializes loader to parse babel
+				loader: 'babel-loader',
+				query: {
+					// Take files to parse through react, then run through es2015
+					presets: ['react', 'es2015']
+				},
+				// Regex to specify which files to get
+				test: /\.jsx?$/,
+				// Specify which folders to exclude
+				exclude: /(node_modules|bower_components)/
+			}
+		]
 	}
 };
